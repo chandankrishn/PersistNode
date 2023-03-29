adf6c060b93bbed5a808084cbf0573b082ef4dba
Channdan Krishhnani <chandan.krishnani@chicmic.con.in>
2023-03-29 13:53:39 +0530

    restructure

--- assets/assets/Script/ChangeScene.ts
+++ assets/assets/Script/ChangeScene.ts
@@ -4,12 +4,16 @@ const { ccclass, property } = _decorator;
 @ccclass("ChangeScene")
 export class ChangeScene extends Component {
   start() {}
+  onLoad() {
+    let persistNode = director.getScene().getChildByName("Perisit");
+    console.log("scene and children", persistNode);
+  }
   changeScene() {
-    director.loadScene("Demo");  
-}
-updateScene(){
-    director.loadScene("test")
-}
+    director.loadScene("Demo");
+  }
+  updateScene() {
+    director.loadScene("test");
+  }
 
   update(deltaTime: number) {}
 }
